217b0199615a0b2ce5c656af0bc100c839fe3d23
dell <878187355@qq.com>
2019-03-24 17:44:01 +0800

    DES

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const fs =require('fs')
 
 http.createServer(function (request,response){
   console.log('request come',request.url)
-  const html =fs.readFileSync('test.html','utf8')
-  // 如果这里不是utf8 识别二进制
-  response.writeHead(200,{
-    'Content-Type': 'text/plain'
-  })
-  response.end(html)
+  
+  if (request.url === '/'){
+    const html = fs.readFileSync('test.html', 'utf8')
+    // 如果这里不是utf8 识别二进制
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    response.end(html)
+  }
+  // 做一个路径的判断
+  if (request.url === '/script.js'){
+    response.writeHead(200, {
+      'Content-Type': 'text/javascript',
+      'Cache-Control': 'max-age-200'
+    })
+    response.end('console.log("script loaded")')
+  }
 }).listen(8888)
 
 console.log('Server listening on 8888')
--- server2.js
+++ server2.js
@@ -4,9 +4,14 @@ http.createServer(function (request, response) {
   console.log('request come', request.url)
   //如果这里不是utf8 识别二进制
   // 200是api要求返回的状态
-  // response.writeHead(200,{
-  //   'Access-Control-Allow-Origin':'*'
-  // })
+  response.writeHead(200,{
+    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Headers': 'X-Test-Cors',
+    'Access-Control-Allow-Methods': 'POST,PUT,Delete',
+    'Access-Control-Max-Age': '1000',
+
+
+  })
   response.end('123')
 }).listen(8887)
 
